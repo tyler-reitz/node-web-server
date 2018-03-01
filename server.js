8a82a7ec1b8af3044e8e3a797290baee5f9a67d9
Tyler Reitz <tdreitz@gmail.com>
2018-02-28 19:21:12 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const fs = require('fs')
+
+const app = express()
+
+app.use(express.static(__dirname + '/public'))
+app.set('view engine', 'hbs')
+
+app.use((req, res, next) => {
+  const now = new Date()
+  const log = `${now}: ${req.method} ${req.url}`
+
+  console.log(log)
+  fs.appendFile('server.log', log + '\n', err => {
+    if (err) {
+      console.log('Unable to append to server.log')
+    }
+  })
+  next()
+})
+
+app.use((req, res, next) => {
+  res.render('maintenanc.hbs')
+})
+
+app.get('/', (req, res) => { 
+  res.send({
+    body: {
+      username: 'tdreitz',
+      password: 'foobar'
+    }
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  })
+})
+
+app.listen(3000, () => 'listening on port 3000')
